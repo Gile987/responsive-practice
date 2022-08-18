f3f4c8da93e51574d370d9985db2aa3e08f42dc5
gile987 <mladen.reljic87@gmail.com>
2022-08-18 16:25:32 +0200

    Add an overlay that prevents scrolling while the hamburger menu is open

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const button = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
 
 button.addEventListener('click', () => {
   button.classList.toggle('open');
+  overlay.classList.toggle('overlay-visible');
+  document.body.classList.toggle('stop-scrolling');
 });
