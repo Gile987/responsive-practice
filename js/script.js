c3e54f51d278138e227d673e344eb60b706a6632
gile987 <mladen.reljic87@gmail.com>
2022-08-18 17:05:43 +0200

    Add mobile menu, style it, and write a script that shows/hides it on hamburger click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const button = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
 
 button.addEventListener('click', () => {
   button.classList.toggle('open');
   overlay.classList.toggle('overlay-visible');
   document.body.classList.toggle('stop-scrolling');
+  menu.classList.toggle('show-menu');
 });
