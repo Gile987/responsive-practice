ae9d1b4c37da490f9d8582a7efa0905386f153dd
gile987 <mladen.reljic87@gmail.com>
2022-08-19 15:33:40 +0200

    Add a script that updates the counter up to the number set in data-target

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const button = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
 
 button.addEventListener('click', () => {
   button.classList.toggle('open');
@@ -8,3 +9,27 @@ button.addEventListener('click', () => {
   document.body.classList.toggle('stop-scrolling');
   menu.classList.toggle('show-menu');
 });
+
+const countUp = () => {
+  counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const start = +counter.innerText;
+      const step = target / 100;
+      const end = target;
+
+      if (start < end) {
+        counter.innerText = Math.ceil(start + step);
+        setTimeout(updateCount, 1);
+      } else {
+        counter.innerText = target;
+      };
+    };
+
+    updateCount();
+  });
+}
+
+countUp();
